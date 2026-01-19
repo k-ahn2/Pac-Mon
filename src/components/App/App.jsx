d26725bfe59fcdabe5693f56505a80d44691cf43
Kevin <kevin@m0ahn.co.uk>
2026-01-19 22:53:49 +0000

    Hide filters fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -729,7 +729,7 @@ function App() {
               </Col>
             }
           </Row>
-          <Row>
+          <Row style={{ display: toggleFilters ? 'none' : 'flex' }}>
             <Col>
                 <Form.Check
                   type="switch"
@@ -739,7 +739,7 @@ function App() {
                 />            
             </Col>
           </Row>
-          <Row>
+          <Row style={{ display: toggleFilters ? 'none' : 'flex' }}>
             <Col>
               <Badge style={{ padding: '0.5em 0em', width: '100%', marginBottom: '0rem'}} bg={"secondary"}>
                 {filteredTraces.length.toLocaleString()} Traces After Filtering
